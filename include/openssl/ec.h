fe4082d55447ad900d0a5c97f42c0f3ef8254590
Rich Salz <rsalz@akamai.com>
2024-12-08 15:33:28 -0500

    Remove `symhacks.h` file

--- include/openssl/ec.h
+++ include/openssl/ec.h
@@ -93,7 +93,6 @@ const char *OSSL_EC_curve_nid2name(int nid);
 # endif
 # ifndef OPENSSL_NO_EC
 #  include <openssl/asn1.h>
-#  include <openssl/symhacks.h>
 #  ifndef OPENSSL_NO_DEPRECATED_1_1_0
 #   include <openssl/bn.h>
 #  endif
