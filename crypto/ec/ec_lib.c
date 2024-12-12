88ace119826370f308ff0c726df6507e321baa24
Theo Buehler <tb@openbsd.org>
2024-12-12 23:32:12 +0100

    Simplify EC_GROUP_get_basis_type()
    With EC2m gone, this will always return 0, so make that a bit more
    obvious.

--- crypto/ec/ec_lib.c
+++ crypto/ec/ec_lib.c
@@ -1289,25 +1289,7 @@ int ossl_ec_point_blind_coordinates(const EC_GROUP *group, EC_POINT *p,
 
 int EC_GROUP_get_basis_type(const EC_GROUP *group)
 {
-    int i;
-
-    if (EC_GROUP_get_field_type(group) != NID_X9_62_characteristic_two_field)
-        /* everything else is currently not supported */
-        return 0;
-
-    /* Find the last non-zero element of group->poly[] */
-    for (i = 0;
-         i < (int)OSSL_NELEM(group->poly) && group->poly[i] != 0;
-         i++)
-        continue;
-
-    if (i == 4)
-        return NID_X9_62_ppBasis;
-    else if (i == 2)
-        return NID_X9_62_tpBasis;
-    else
-        /* everything else is currently not supported */
-        return 0;
+    return 0;
 }
 
 #ifndef FIPS_MODULE
