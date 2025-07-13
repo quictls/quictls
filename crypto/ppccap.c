1ce079095c905dd0248362d695134bb308426e50
Rich Salz <rsalz@akamai.com>
2025-07-13 04:14:41 -0400

    Remove some unused functions (#262)
    `OPENSSL_wipe_cpu` was internal and never used.
    `OPENSSSL_instrument_{bus,bus2}` are removed.
    
    Inspired by OpenSSL PRs 27288 by jsondevers and 27362 by
    Teddy Engel

--- crypto/ppccap.c
+++ crypto/ppccap.c
@@ -60,32 +60,6 @@ uint32_t OPENSSL_rdtsc(void)
         return 0;
 }
 
-size_t OPENSSL_instrument_bus_mftb(unsigned int *, size_t);
-size_t OPENSSL_instrument_bus_mfspr268(unsigned int *, size_t);
-
-size_t OPENSSL_instrument_bus(unsigned int *out, size_t cnt)
-{
-    if (OPENSSL_ppccap_P & PPC_MFTB)
-        return OPENSSL_instrument_bus_mftb(out, cnt);
-    else if (OPENSSL_ppccap_P & PPC_MFSPR268)
-        return OPENSSL_instrument_bus_mfspr268(out, cnt);
-    else
-        return 0;
-}
-
-size_t OPENSSL_instrument_bus2_mftb(unsigned int *, size_t, size_t);
-size_t OPENSSL_instrument_bus2_mfspr268(unsigned int *, size_t, size_t);
-
-size_t OPENSSL_instrument_bus2(unsigned int *out, size_t cnt, size_t max)
-{
-    if (OPENSSL_ppccap_P & PPC_MFTB)
-        return OPENSSL_instrument_bus2_mftb(out, cnt, max);
-    else if (OPENSSL_ppccap_P & PPC_MFSPR268)
-        return OPENSSL_instrument_bus2_mfspr268(out, cnt, max);
-    else
-        return 0;
-}
-
 #if defined(__GLIBC__) && defined(__GLIBC_PREREQ)
 # if __GLIBC_PREREQ(2, 16)
 #  include <sys/auxv.h>
