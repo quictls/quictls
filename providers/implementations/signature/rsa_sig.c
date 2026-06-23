1c1293bdfbeb3c662c3bb81b3d83aa13fadb1506
Ilya Shipitsin <chipitsine@gmail.com>
2026-06-23 18:16:09 +0200

    fix string formatting warnings (#351)
    * bio: fix invalid type in format specifier in file_ctrl
    
    Address Coverity CID 552658: cast `void *ptr` to `(const char *)`
    when calling `ERR_raise_data` with a `%s` format specifier.
    
    * evp: fix invalid type in format specifier in fix_rsa_padding_mode
    
    Address Coverity CID 552668: pass `(const char *)ctx->p2` instead of
    `ctx->p1` when printing padding name string via the `%s` format specifier.
    
    * rsa: fix invalid type in format specifier in rsa_sign
    
    Address Coverity CID 553925: use `%zu` instead of `%d` for `tbslen + 1`
    which has type `size_t`.
    
    * kdfs: fix invalid type and arguments in argon2.c thread pool error
    
    Address Coverity CID 553927: correct the format string and arguments
    when reporting an invalid thread pool size error. Format requested
    threads with `%u` and available threads with `%ju` (casting the
    returned `uint64_t` value to `uintmax_t`).
    
    * rsa: fix invalid type in digest length error format specifier
    
    Address Coverity CID 553933: use `%zu` instead of `%d` for `mdsize`
    and `tbslen` in `rsa_verify` as both variables have type `size_t`.
    
    * rsa: fix invalid type in output buffer error format specifier
    
    Address Coverity CID 553934: use `%zu` instead of `%d` for the `routsize`
    argument in `rsa_verify_recover` since it is of type `size_t`.

--- providers/implementations/signature/rsa_sig.c
+++ providers/implementations/signature/rsa_sig.c
@@ -574,7 +574,7 @@ static int rsa_sign(void *vprsactx, unsigned char *sig, size_t *siglen,
         case RSA_X931_PADDING:
             if ((size_t)RSA_size(prsactx->rsa) < tbslen + 1) {
                 ERR_raise_data(ERR_LIB_PROV, PROV_R_KEY_SIZE_TOO_SMALL,
-                               "RSA key size = %d, expected minimum = %d",
+                               "RSA key size = %d, expected minimum = %zu",
                                RSA_size(prsactx->rsa), tbslen + 1);
                 return 0;
             }
@@ -722,7 +722,7 @@ static int rsa_verify_recover(void *vprsactx,
             if (rout != prsactx->tbuf) {
                 if (routsize < (size_t)ret) {
                     ERR_raise_data(ERR_LIB_PROV, PROV_R_OUTPUT_BUFFER_TOO_SMALL,
-                                   "buffer size is %d, should be %d",
+                                   "buffer size is %zu, should be %d",
                                    routsize, ret);
                     return 0;
                 }
@@ -805,7 +805,7 @@ static int rsa_verify(void *vprsactx, const unsigned char *sig, size_t siglen,
                 mdsize = rsa_get_md_size(prsactx);
                 if (tbslen != mdsize) {
                     ERR_raise_data(ERR_LIB_PROV, PROV_R_INVALID_DIGEST_LENGTH,
-                                   "Should be %d, but got %d",
+                                   "Should be %zu, but got %zu",
                                    mdsize, tbslen);
                     return 0;
                 }
